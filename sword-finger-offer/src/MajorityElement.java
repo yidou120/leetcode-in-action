94679fe7fd13e7ce95ad4d1d9cd33af6a23e7214
yidou120 <1596510418@qq.com>
2020-04-27 17:35:30 +0800

    数组中出现次数超过一半的数字/使用hash法

---
+++ sword-finger-offer/src/MajorityElement.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+/**
+ * @ClassName MajorityElement
+ * @Description 数组中出现次数超过一半的数字
+ * @Author 中森明菜
+ * @Date 2020/4/27 14:19
+ * @Version 1.0
+ */
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i],1);
+            }else{
+                Integer count = map.get(nums[i]);
+                map.put(nums[i],count++);
+            }
+        }
+        Set<Map.Entry<Integer, Integer>> entries = map.entrySet();
+        Collection<Integer> values = map.values();
+        Object[] objects = values.toArray();
+        Arrays.sort(objects);
+        for(Map.Entry<Integer,Integer> entry:entries){
+            if(entry.getValue()==(Integer)objects[objects.length-1]){
+                return entry.getKey();
+            }
+        }
+        return -1;
+    }
+}
