7b4207636d5f921d24ba1fa910d39cd426817b3e
杨治法 <1596510418@qq.com>
2020-10-14 16:41:41 +0800

    计算数组中所有奇数子数组之和

--- sword-finger-offer/src/array/SumOddLengthSubarrays.java
+++ sword-finger-offer/src/array/SumOddLengthSubarrays.java
@@ -13,12 +13,21 @@ public class SumOddLengthSubarrays {
     // 1,4,2 4,2,5 2,5,3
     // 1,4,2,5,3
     public int sumOddLengthSubarrays(int[] arr) {
+        // 计算每个数字出现的次数
+        // 总和就是次数*数值
         int len = arr.length;
         int result = 0;
-        for (int i=0;i<len;i++){
-            for (int j=i;j<len;j+=2){
-                result += arr[j];
-            }
+        for (int i=0;i<len-1;i++){
+            // 左奇 number 右奇
+            // 左偶 number 右偶
+            // 左边可以选择的方案 可以选0,1,2..个数字
+            int left = i + 1;
+            int right = len - i;
+            int leftEven = (left + 1)/2;
+            int rightEven = (right + 1)/2;
+            int leftOdd = left/2;
+            int rightOdd = right/2;
+            result +=  (leftEven*rightEven+leftOdd*rightOdd) * arr[i];
         }
         return result;
     }
